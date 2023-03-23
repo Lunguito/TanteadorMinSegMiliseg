79ef91a84ecafdd9b04e3c2eb358bed36ec44a2a
Lungo <102230600+Lunguito@users.noreply.github.com>
2023-03-23 17:17:57 -0300

    Add files via upload

---
+++ TanteadorTiempo/TanteadorTiempo/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace TanteadorTiempo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblMinutos = new System.Windows.Forms.Label();
+            this.lblSegundos = new System.Windows.Forms.Label();
+            this.lblMilisegundos = new System.Windows.Forms.Label();
+            this.btnComenzar = new System.Windows.Forms.Button();
+            this.btnPausar = new System.Windows.Forms.Button();
+            this.btnResetear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMinutos
+            // 
+            this.lblMinutos.AutoSize = true;
+            this.lblMinutos.Font = new System.Drawing.Font("Comic Sans MS", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMinutos.Location = new System.Drawing.Point(37, 51);
+            this.lblMinutos.Name = "lblMinutos";
+            this.lblMinutos.Size = new System.Drawing.Size(149, 49);
+            this.lblMinutos.TabIndex = 0;
+            this.lblMinutos.Text = "Minutos";
+            // 
+            // lblSegundos
+            // 
+            this.lblSegundos.AutoSize = true;
+            this.lblSegundos.Font = new System.Drawing.Font("Comic Sans MS", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSegundos.Location = new System.Drawing.Point(192, 51);
+            this.lblSegundos.Name = "lblSegundos";
+            this.lblSegundos.Size = new System.Drawing.Size(176, 49);
+            this.lblSegundos.TabIndex = 1;
+            this.lblSegundos.Text = "Segundos";
+            // 
+            // lblMilisegundos
+            // 
+            this.lblMilisegundos.AutoSize = true;
+            this.lblMilisegundos.Font = new System.Drawing.Font("Comic Sans MS", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMilisegundos.Location = new System.Drawing.Point(361, 51);
+            this.lblMilisegundos.Name = "lblMilisegundos";
+            this.lblMilisegundos.Size = new System.Drawing.Size(137, 49);
+            this.lblMilisegundos.TabIndex = 2;
+            this.lblMilisegundos.Text = "Miliseg";
+            // 
+            // btnComenzar
+            // 
+            this.btnComenzar.Location = new System.Drawing.Point(111, 148);
+            this.btnComenzar.Name = "btnComenzar";
+            this.btnComenzar.Size = new System.Drawing.Size(75, 23);
+            this.btnComenzar.TabIndex = 3;
+            this.btnComenzar.Text = "Iniciar";
+            this.btnComenzar.UseVisualStyleBackColor = true;
+            this.btnComenzar.Click += new System.EventHandler(this.btnComenzar_Click);
+            // 
+            // btnPausar
+            // 
+            this.btnPausar.Location = new System.Drawing.Point(232, 148);
+            this.btnPausar.Name = "btnPausar";
+            this.btnPausar.Size = new System.Drawing.Size(75, 23);
+            this.btnPausar.TabIndex = 4;
+            this.btnPausar.Text = "Pausar";
+            this.btnPausar.UseVisualStyleBackColor = true;
+            this.btnPausar.Click += new System.EventHandler(this.btnPausar_Click);
+            // 
+            // btnResetear
+            // 
+            this.btnResetear.Location = new System.Drawing.Point(347, 148);
+            this.btnResetear.Name = "btnResetear";
+            this.btnResetear.Size = new System.Drawing.Size(75, 23);
+            this.btnResetear.TabIndex = 5;
+            this.btnResetear.Text = "Resetear";
+            this.btnResetear.UseVisualStyleBackColor = true;
+            this.btnResetear.Click += new System.EventHandler(this.btnResetear_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(549, 274);
+            this.Controls.Add(this.btnResetear);
+            this.Controls.Add(this.btnPausar);
+            this.Controls.Add(this.btnComenzar);
+            this.Controls.Add(this.lblMilisegundos);
+            this.Controls.Add(this.lblSegundos);
+            this.Controls.Add(this.lblMinutos);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblMinutos;
+        private System.Windows.Forms.Label lblSegundos;
+        private System.Windows.Forms.Label lblMilisegundos;
+        private System.Windows.Forms.Button btnComenzar;
+        private System.Windows.Forms.Button btnPausar;
+        private System.Windows.Forms.Button btnResetear;
+    }
+}
+
---
+++ TanteadorTiempo/TanteadorTiempo/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TanteadorTiempo
+{
+    public partial class Form1 : Form
+    {
+        private int minutos = 0;
+        private int segundos = 0;
+        private int milisegundos = 0;
+        private bool corriendo = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 10;
+            timer1.Tick += new EventHandler(timer1_Tick);
+        }
+
+        private void btnComenzar_Click(object sender, EventArgs e)
+        {
+            corriendo = true;
+            timer1.Start();
+        }
+
+        private void btnPausar_Click(object sender, EventArgs e)
+        {
+            corriendo = false;
+            timer1.Stop();
+        }
+
+        private void btnResetear_Click(object sender, EventArgs e)
+        {
+            corriendo = false;
+            minutos = 0;
+            segundos = 0;
+            milisegundos = 0;
+            ActualizarEtiquetas();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            milisegundos += 10;
+
+            if (milisegundos >= 1000)
+            {
+                segundos++;
+                milisegundos = 0;
+            }
+
+            if (segundos >= 60)
+            {
+                minutos++;
+                segundos = 0;
+            }
+
+            ActualizarEtiquetas();
+        }
+
+        private void ActualizarEtiquetas()
+        {
+            lblMinutos.Text = minutos.ToString("00");
+            lblSegundos.Text = segundos.ToString("00");
+            lblMilisegundos.Text = milisegundos.ToString("000");
+        }
+    }
+}
